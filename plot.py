6c72c307dab9c67d8cba8e3c62da4af16f87e439
gwulf <gideon.wulfsohn@gmail.com>
2014-11-28 18:12:29 -0500

    change formating to pep8

--- plot.py
+++ plot.py
@@ -4,26 +4,23 @@ import matplotlib.pyplot as plt
 import pdb
 
 files = [ "Positive_10k.json", "headProblemsExtended_10k.json",
-"a_10k.json", "hearLoud_10k.json",
-"an_10k.json", "hearSoft_10k.json",
-"discrepancy_10k.json", "hearSpeak_10k.json",
-"exclusives_10k.json", "negative_10k.json",
-"fillers_10k.json", "negativemarkers_10k.json",
-"fillersyouth_10k.json", "the_10k.json",
-"headProblems_10k.json", "think_10k.json" ]
+          "a_10k.json", "hearLoud_10k.json",
+          "an_10k.json", "hearSoft_10k.json",
+          "discrepancy_10k.json", "hearSpeak_10k.json",
+          "exclusives_10k.json", "negative_10k.json",
+          "fillers_10k.json", "negativemarkers_10k.json",
+          "fillersyouth_10k.json", "the_10k.json",
+          "headProblems_10k.json", "think_10k.json" ]
 
 for filename in files:
-	f = open('results/{}'.format(filename), 'r')
-	r = json.load(f)
-	f.close()
+    with open('results/{}'.format(filename), 'r') as f:
+        r = json.load(f)
 
-	i = 0
-	counts = []
-	wordnum = []
-	x = ['0','10000','20000','30000','40000','50000','60000','70000']
-	for indx in x:
-    		counts.append(r[indx]['+'])
-    		wordnum.append(int(indx))
-    	plt.scatter(wordnum, counts)
-    	plt.savefig('plots/{}.png'.format(filename[:-5]))
-        plt.close()
+    counts = []
+    wordnum = []
+    for key, value in r.items():
+        wordnum.append(int(key))
+        counts.append(value.get('+', 0))
+    plt.scatter(wordnum, counts)
+    plt.savefig('plots/{}.png'.format(filename[:-5]))
+    plt.close()
